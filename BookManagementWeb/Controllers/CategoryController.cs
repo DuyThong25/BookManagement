284b8ad908a3da5e7e56965f71923773d884a61b
duythong2505@gmail.com <duythong2505@gmail.com>
2024-05-16 10:24:16 +0700

    - Create partial view to show Toastr

--- BookManagementWeb/Controllers/CategoryController.cs
+++ BookManagementWeb/Controllers/CategoryController.cs
@@ -14,7 +14,7 @@ namespace BookManagementWeb.Controllers
         public IActionResult Index()
         {
 
-            return View(_db.Categories.ToList());
+            return View(_db.Categories.OrderBy(x => x.DisplayOrder).ToList());
         }
 
         public IActionResult Create()
@@ -32,9 +32,64 @@ namespace BookManagementWeb.Controllers
             {
                 _db.Categories.Add(category);
                 _db.SaveChanges();
+                TempData["success"] = "Category add successfully";
                 return RedirectToAction("Index");
             }
             return View();
+        }
+
+        public IActionResult Edit(int id)
+        {
+            Category? category = _db.Categories.Find(id);
+            if(category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (category.Name == category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "Category Name không được trùng với Display Order");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(category);
+                _db.SaveChanges();
+                TempData["success"] = "Category edit successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int id)
+        {
+            Category? category = _db.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirm(int id)
+        {
+            Category? category = _db.Categories.Find(id);
+            if(category != null)
+            {
+                _db.Categories.Remove(category);
+                _db.SaveChanges();
+                TempData["success"] = "Category delete successfully";
+                return RedirectToAction("Index");
+
+            }else
+            {
+                return NotFound();
+            }
         }
     }
 }
