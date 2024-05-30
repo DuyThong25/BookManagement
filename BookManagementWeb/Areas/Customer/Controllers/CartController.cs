6db731a1f04ad12f783cb8c0009c9c93898c757e
duythong2505@gmail.com <duythong2505@gmail.com>
2024-05-30 15:35:54 +0700

    Use AJAX to update cart

--- BookManagementWeb/Areas/Customer/Controllers/CartController.cs
+++ BookManagementWeb/Areas/Customer/Controllers/CartController.cs
@@ -40,19 +40,121 @@ namespace BookManagementWeb.Areas.Customer.Controllers
 
         private double GetBasePrice(ShoppingCart shoppingCart)
         {
-            if(shoppingCart.Count < 50)
+            if (shoppingCart.Count < 50)
             {
                 return shoppingCart.Product.Price;
-            }else
+            }
+            else
             {
-                if(shoppingCart.Count < 100)
+                if (shoppingCart.Count < 100)
                 {
                     return shoppingCart.Product.Price50;
-                }else
+                }
+                else
                 {
                     return shoppingCart.Product.Price100;
                 }
             }
         }
+
+        private double GetOrderTotal(ShoppingCart cartUpdate, bool isDelete)
+        {
+            var claimsIdentity = User.Identity as ClaimsIdentity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            ShoppingCartVM = new ShoppingCartVM();
+            if (isDelete == true)
+            {
+                ShoppingCartVM.ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(x => x.ApplicationUserId == userId && x.Id != cartUpdate.Id, includeProperties: "Product");
+            }
+            else
+            {
+                ShoppingCartVM.ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(x => x.ApplicationUserId == userId, includeProperties: "Product");
+            }
+
+            foreach (var cart in ShoppingCartVM.ShoppingCartList)
+            {
+                cart.Price = GetBasePrice(cart);
+                if (cart.Id == cartUpdate.Id)
+                {
+                    // This is a cart being update count
+                    ShoppingCartVM.OrderTotal += (cart.Price * cartUpdate.Count);
+                }
+                else
+                {
+                    ShoppingCartVM.OrderTotal += (cart.Price * cart.Count);
+                }
+            }
+            return ShoppingCartVM.OrderTotal;
+        }
+        #region Api Method
+
+        //[HttpGet]
+        //public IActionResult GetAll(int cartId)
+        //{
+        //    var cartFromDb = _unitOfWork.ShoppingCart.GetAll(x => x.Id == cartId);
+        //    return Json(new { cart = cartFromDb });
+        //}
+        [HttpPut("customer/cart/plus/{cartId}")]
+        public IActionResult Plus(int cartId)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(x => x.Id == cartId, includeProperties: "Product");
+
+            if (cartFromDb != null)
+            {
+                cartFromDb.Price = GetBasePrice(cartFromDb);
+                cartFromDb.Count += 1;
+                double orderTotal = GetOrderTotal(cartFromDb, false);
+                _unitOfWork.ShoppingCart.Update(cartFromDb);
+                _unitOfWork.Save();
+                return Json(new { cart = cartFromDb, total = orderTotal });
+            }
+            return NotFound();
+        }
+
+        [HttpPut("customer/cart/minus/{cartId}")]
+        public IActionResult Minus(int cartId)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(x => x.Id == cartId, includeProperties: "Product");
+
+            if (cartFromDb != null)
+            {
+                cartFromDb.Price = GetBasePrice(cartFromDb);
+                cartFromDb.Count -= 1;
+                double orderTotal;
+                if (cartFromDb.Count == 0)
+                {
+                    // Remove cart
+                    orderTotal = GetOrderTotal(cartFromDb, isDelete: true);
+                    _unitOfWork.ShoppingCart.Remove(cartFromDb);
+                    _unitOfWork.Save();
+                    return Json(new { cart = cartFromDb, total = orderTotal });
+                }
+                else
+                {
+                    orderTotal = GetOrderTotal(cartFromDb, isDelete: false);
+                    _unitOfWork.ShoppingCart.Update(cartFromDb);
+                    _unitOfWork.Save();
+                    return Json(new { cart = cartFromDb, total = orderTotal });
+                }
+            }
+            return NotFound();
+        }
+
+        [HttpDelete("customer/cart/delete/{cartId}")]
+        public IActionResult Delete(int cartId)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(x => x.Id == cartId, includeProperties: "Product");
+            if (cartFromDb != null)
+            {
+                double orderTotal = GetOrderTotal(cartFromDb, isDelete: true);
+                _unitOfWork.ShoppingCart.Remove(cartFromDb);
+                _unitOfWork.Save();
+                return Json(new { cart = cartFromDb, total = orderTotal });
+            }
+            return NotFound();
+        }
+
+        #endregion
     }
 }
