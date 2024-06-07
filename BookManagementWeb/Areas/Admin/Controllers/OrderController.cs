7b65204da3039fec492b25752abb2a4921497927
duythong2505@gmail.com <duythong2505@gmail.com>
2024-06-07 18:51:13 +0700

    add OrderVM, Order List UI

---
+++ BookManagementWeb/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,60 @@
+﻿using BookManager.DataAccess.Repository.IRepository;
+using BookManager.Models.ViewModel;
+using BookManager.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookManagementWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = StaticDetail.Role_Admin)]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser"));
+        }
+
+        public IActionResult Details(int orderId)
+        {
+            OrderVM? orderVM = new()
+            {
+                OrderHeader = _unitOfWork.OrderHeader.Get(x => x.Id == orderId, includeProperties: "ApplicationUser"),
+                OrderDetails = _unitOfWork.OrderDetail.GetAll(x => x.OrderHeaderId == orderId).ToList()
+            };
+
+            return View(orderVM);
+        }
+
+        #region API Method
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            //OrderVM? orderVM = _unitOfWork.Product.Get(x => x.ProductId == id);
+            OrderVM? orderVM = new()
+            {
+                OrderHeader = _unitOfWork.OrderHeader.Get(x => x.Id == id, includeProperties: "ApplicationUser"),
+                OrderDetails = _unitOfWork.OrderDetail.GetAll(x => x.OrderHeaderId == id).ToList()
+            };
+
+            if (orderVM != null)
+            {
+                //_unitOfWork.Product.Remove(product);
+                //_unitOfWork.Save();
+                return Json(new { success = true, message = "Delete Succesful" });
+            }
+            else
+            {
+                return Json(new { success = false, message = "Delete Fail" });
+            }
+        }
+        #endregion
+    }
+}
