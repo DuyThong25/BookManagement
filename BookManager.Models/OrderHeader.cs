242682a37074c8cf0286dd2318339e8c15f033ff
duythong2505@gmail.com <duythong2505@gmail.com>
2024-06-16 14:33:25 +0700

    Update small in OrderHeader and add Migration

--- BookManager.Models/OrderHeader.cs
+++ BookManager.Models/OrderHeader.cs
@@ -38,18 +38,18 @@ namespace BookManager.Models
 		public string Name { get; set; }
 		[Required]
 
-		public string? Address { get; set; }
+		public string Address { get; set; }
 		[Required]
 
-		public string? Ward { get; set; }
+		public string Ward { get; set; }
 		[Required]
 
-		public string? District { get; set; }
+		public string District { get; set; }
 		[Required]
 
-		public string? City { get; set; }
+		public string City { get; set; }
 		[Required]
-		public string? PhoneNumber { get; set; }
+		public string PhoneNumber { get; set; }
 
 		[ForeignKey("ApplicationUserId")]
 		[ValidateNever]
