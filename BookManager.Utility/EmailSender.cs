dca251763172a303f166677a6cfa284cfc0be8e4
duythong2505@gmail.com <duythong2505@gmail.com>
2024-06-21 18:59:59 +0700

    Config MailMessage

--- BookManager.Utility/EmailSender.cs
+++ BookManager.Utility/EmailSender.cs
@@ -1,18 +1,55 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace BookManager.Utility
 {
     public class EmailSender : IEmailSender
     {
+        private readonly IConfiguration _configuration;
+        public EmailSender(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            // logic send email
-            return Task.CompletedTask;
+            try
+            {
+                // logic send email
+                MailMessage mailMessage = new(_configuration["NetMail:sender"], email)
+                {
+                    IsBodyHtml = true,
+                    Subject = subject,
+                    Body = htmlMessage
+                };
+                SmtpClient client = new SmtpClient
+                {
+                    Port = _configuration.GetValue<int>("NetMail:port"),
+                    Host = _configuration["NetMail:smtpHost"],
+                    EnableSsl = true,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential()
+                    {
+                        UserName = _configuration["NetMail:sender"],
+                        Password = _configuration["NetMail:senderpassword"]
+                    }
+                };
+
+                return client.SendMailAsync(mailMessage);
+            }
+            catch (Exception ex)
+            {
+                return Task.FromException(ex);
+            }
         }
     }
 }
