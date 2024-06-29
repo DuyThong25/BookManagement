2ca64a8b5951a188e145207d8954f69659fb6fff
duythong2505@gmail.com <duythong2505@gmail.com>
2024-06-29 18:32:45 +0700

    Handle Update Pemission to user

--- BookManagementWeb/Areas/Admin/Controllers/UserController.cs
+++ BookManagementWeb/Areas/Admin/Controllers/UserController.cs
@@ -5,6 +5,7 @@ using BookManager.Models.ViewModel;
 using BookManager.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -18,9 +19,14 @@ namespace BookManagementWeb.Areas.Admin.Controllers
     public class UserController : Controller
     {
         private readonly ApplicationDbContext _db;
-        public UserController(ApplicationDbContext db)
+        private readonly UserManager<IdentityUser> _userManager;
+        [BindProperty]
+        public RoleManagerVM RoleManager { get; set; }
+
+        public UserController(ApplicationDbContext db, UserManager<IdentityUser> userManager)
         {
             _db = db;
+            _userManager = userManager;
         }
         public IActionResult Index()
         {
@@ -39,67 +45,96 @@ namespace BookManagementWeb.Areas.Admin.Controllers
             return View(listUserFromDB);
         }
 
-        //public IActionResult CreateOrUpdate(int? id)
-        //{
-        //    Company company = new Company();
-        //    if (id != null && id != 0) // update
-        //    {
-        //        company = _unitOfWork.Company.Get(x => x.Id == id);
-        //    }
-        //    return View(company);
+        public IActionResult Pemission(string id)
+        {
+            string roleID = _db.UserRoles.FirstOrDefault(x => x.UserId == id).RoleId;
+            RoleManager = new()
+            {
+                ApplicationUser = _db.ApplicationUsers.Include(x => x.Company).FirstOrDefault(x => x.Id == id),
+                RoleList = _db.Roles.Select(x => new SelectListItem
+                {
+                    Text = x.Name,
+                    Value = x.Name
+                }),
+                CompanyList = _db.Companies.Select(x => new SelectListItem
+                {
+                    Text = x.Name,
+                    Value = x.Id.ToString()
+                })
+            };
 
-        //}
-        //[HttpPost]
-        //public IActionResult CreateOrUpdate(Company company)
-        //{
-        //    if (ModelState.IsValid)
-        //    {        
+            RoleManager.ApplicationUser.Role = _db.Roles.FirstOrDefault(x => x.Id == roleID).Name;
+            return View(RoleManager);
+        }
 
-        //        if(company.Id == 0) // Create
-        //        {
-        //            _unitOfWork.Company.Add(company);
-        //            _unitOfWork.Save();
-        //            TempData["success"] = "Company add successfully";
-        //        }else // update
-        //        {
-        //            _unitOfWork.Company.Update(company);
-        //            _unitOfWork.Save();
-        //            TempData["success"] = "Company edit successfully";
-        //        }
-        //        return RedirectToAction("Index");
-        //    }
-        //    else
-        //    {       
-        //        TempData["error"] = "Something went wrong..";
-        //        return View(company);
-        //    }
-        //}
+        [HttpPost]
+        public IActionResult Pemission()
+        {
+            string roleID = _db.UserRoles.FirstOrDefault(x => x.UserId == RoleManager.ApplicationUser.Id).RoleId;
+            string oldRole = _db.Roles.FirstOrDefault(x => x.Id == roleID).Name;
+
+            // Update happening
+            if (RoleManager.ApplicationUser.Role != oldRole)
+            {
+                var applicationUser = _db.ApplicationUsers.FirstOrDefault(x => x.Id == RoleManager.ApplicationUser.Id);
+                if (RoleManager.ApplicationUser.Role == StaticDetail.Role_Company)
+                {
+                    applicationUser.CompanyID = RoleManager.ApplicationUser.CompanyID;
+                }
+                //if old role == role Company -> new role not be company
+                if (oldRole == StaticDetail.Role_Company)
+                {
+                    applicationUser.CompanyID = null;
+                }
+
+                _db.Update(applicationUser);
+                _db.SaveChanges();
+
+                // Update UserRole table
+                _userManager.RemoveFromRoleAsync(applicationUser, oldRole).GetAwaiter().GetResult();
+                _userManager.AddToRoleAsync(applicationUser, RoleManager.ApplicationUser.Role).GetAwaiter().GetResult();
+
+                TempData["success"] = "Update Pemission Successfully.";
+                return RedirectToAction("Index");
+            }
+
+            TempData["error"] = "Update Pemission Fail.";
+            return View(RoleManager);
+        }
+
         #region Api Method
 
-        //[HttpGet]
-        //public IActionResult GetAll()
-        //{
-        //    List<Company> listCompany = _unitOfWork.Company.GetAll().ToList();
-        //    return Json(new {data = listCompany});
-        //}
+        [HttpPost]
+        public IActionResult LockUnlockUser(string id)
+        {
+            var userFromDB = _db.ApplicationUsers.FirstOrDefault(x => x.Id == id);
 
-        //[HttpDelete]
-        //public IActionResult Delete(int id)
-        //{
-        //    Company? company = _unitOfWork.Company.Get(x => x.Id == id);
+            if (userFromDB != null)
+            {
+                // User is currently locked -> Unlock User
+                if (userFromDB.LockoutEnd != null && userFromDB.LockoutEnd > DateTime.Now)
+                {
+                    userFromDB.LockoutEnd = DateTime.Now;
+                    _db.Update(userFromDB);
+                    _db.SaveChanges();
+                    TempData["success"] = "Unlocking User Successfully.";
+                    return Json(new { success = true });
 
-        //    if (company != null)
-        //    {
-        //        _unitOfWork.Company.Remove(company);
-        //        _unitOfWork.Save();
-        //        return Json(new { success = true, message = "Delete Succesful" });
-        //    }
-        //    else
-        //    {
-        //        return Json(new { success = false, message = "Delete Fail" });
-        //    }
-        //}
-
+                }
+                else
+                {
+                    // User is currently unlocked -> Lock User
+                    userFromDB.LockoutEnd = DateTime.Now.AddYears(1000);
+                    _db.Update(userFromDB);
+                    _db.SaveChanges();
+                    TempData["success"] = "locking User Successfully.";
+                    return Json(new { success = true });
+                }
+            }
+            // running here means bug
+            TempData["error"] = "Error while Locking/Unlocking User.";
+            return Json(new { success = false });
+        }
         #endregion
 
     }
