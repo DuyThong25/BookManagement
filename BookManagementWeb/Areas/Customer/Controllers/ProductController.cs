878664c74cc64c886e6cecd454a7712793a97f3f
duythong2505@gmail.com <duythong2505@gmail.com>
2024-08-13 08:55:22 +0700

    Hadle UI, Add ProductFilterVM

--- BookManagementWeb/Areas/Customer/Controllers/ProductController.cs
+++ BookManagementWeb/Areas/Customer/Controllers/ProductController.cs
@@ -1,13 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookManager.DataAccess.Repository.IRepository;
+using BookManager.Models.ViewModel;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookManagementWeb.Areas.Customer.Controllers
 {
     [Area("Customer")]
     public class ProductController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            // Lấy ra danh sách sản phẩm
+            var listProduct = _unitOfWork.Product.GetAll();
+            ProductFilterVM productFilterVM = new()
+            {
+                Products = listProduct,
+                Categories = _unitOfWork.Category.GetAll()
+            };
+            return View(productFilterVM);
+        }
+
+        public IActionResult GetProductListByFilter(ProductFilterVM productFilterVM)
+        {
+
+
+            return PartialView();
         }
     }
 }
---
+++ BookManager.Models/ViewModel/ProductFilterVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookManager.Models.ViewModel
+{
+    public class ProductFilterVM
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public IEnumerable<Category> Categories{ get; set; }
+        public List<int> SelectedCategoryIds { get; set; }
+        public string SearchInput {  get; set; }
+    }
+}
