f273bcf844423de2cce750728372778ddf9486d1
duythong2505@gmail.com <duythong2505@gmail.com>
2024-08-14 10:26:54 +0700

    Query first - Get Product later

--- BookManagementWeb/Areas/Customer/Controllers/ProductController.cs
+++ BookManagementWeb/Areas/Customer/Controllers/ProductController.cs
@@ -1,7 +1,9 @@
-﻿using BookManager.DataAccess.Repository.IRepository;
+﻿using BookManager.DataAccess.Data;
+using BookManager.DataAccess.Repository.IRepository;
 using BookManager.Models;
 using BookManager.Models.ViewModel;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookManagementWeb.Areas.Customer.Controllers
 {
@@ -9,9 +11,11 @@ namespace BookManagementWeb.Areas.Customer.Controllers
     public class ProductController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
-        public ProductController(IUnitOfWork unitOfWork)
+        private readonly ApplicationDbContext _db;
+        public ProductController(IUnitOfWork unitOfWork, ApplicationDbContext db)
         {
             _unitOfWork = unitOfWork;
+            _db = db;
         }
 
         public IActionResult Index()
@@ -28,7 +32,7 @@ namespace BookManagementWeb.Areas.Customer.Controllers
 
         public IActionResult GetProductListByFilter(ProductFilterVM productFilterVM)
         {
-            var query = _unitOfWork.Product.GetAll(includeProperties: "Category,ProductImages").AsQueryable();
+            IQueryable<Product> query = _db.Products.AsNoTracking();
             List<int> selectedCategoryId = productFilterVM.SelectedCategoryIds;
             List<int> PriceRanges = productFilterVM.RangerPrice; // vị trí 0 là min price - vị trí 1 là max price
             if (selectedCategoryId != null)
@@ -41,13 +45,13 @@ namespace BookManagementWeb.Areas.Customer.Controllers
                 query = query.Where(x => x.Title.Trim().ToLower().Contains(searchInput));
 
             }
-            if(productFilterVM.RangerPrice != null)
+            if (productFilterVM.RangerPrice != null)
             {
-               query = query.Where(x => x.Price100 >= PriceRanges[0] &&  x.Price <= PriceRanges[1]);
+                query = query.Where(x => x.Price100 >= PriceRanges[0] && x.Price <= PriceRanges[1]);
             }
             productFilterVM = new()
             {
-                Products = query.ToList(),
+                Products = query.Include("Category").Include("ProductImages").ToList(),
                 Categories = _unitOfWork.Category.GetAll(),
                 SelectedCategoryIds = selectedCategoryId
             };
